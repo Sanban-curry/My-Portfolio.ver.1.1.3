09df82031a28fc03cb65ffab6c95ad98e66fbe2d
Sanban-curry <84472558+Sanban-curry@users.noreply.github.com>
2021-05-25 12:46:38 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+'user strict'
+
+const $tab = document.getElementById('js-tab');
+const $nav = $tab.querySelectorAll('[data-nav]');
+const $content = $tab.querySelectorAll('[data-content]');
+
+const init = () => {
+  $content[0].style.display ='block';
+};
+
+init();
+
+
+const HandleClick = (e) =>{
+  e.preventDefault();
+  const $this =e.target;
+  const targetVal = $this.dataset.nav;
+
+  let index=0;
+  while(index<$nav.length){
+    $content[index].style.display= 'none';
+    $nav[index].classList.remove('is-active');
+    index ++;
+  };
+
+  $tab.querySelectorAll('[data-content="'+targetVal+'"]')[0].style.display ='block';
+  $nav[targetVal].classList.add('is-active');
+};
+
+let index =0;
+while(index<$nav.length){
+  $nav[index].addEventListener('click',(e)=>HandleClick(e));
+  index++;
+};
+
+
